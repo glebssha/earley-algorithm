46ebb29365b14ee1e98aa655f5521dbf28edf2bc
glebssha <61522917+glebssha@users.noreply.github.com>
2020-12-22 04:50:01 +0300

    updt

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+########################################################################################################################
+
+
 class Rule:
     def __init__(self, first, second):
         self.first = first
@@ -5,6 +8,9 @@ class Rule:
 
     def __eq__(self, other):
         return self.first == other.first and self.second == other.second
+
+
+########################################################################################################################
 
 
 class Situation:
@@ -19,19 +25,70 @@ class Situation:
                self.divider == other.divider
 
 
-class Earley:
-    @staticmethod
-    def Scan(situation_list, word, j):
+########################################################################################################################
+
+
+class AlgorithmEarley:
+    def __init__(self, grammar, word):
+        self.word = word
+        self.grammar = grammar
+        self.D = [set() for i in range(len(self.word) + 1)]
+
+    def Scan(self, j):
         if j == 0:
             return
-        for situation in situation_list[j - 1]:
-            if situation.rule.second[situation.divider] == word[j - 1]:
-                situation_list[j].add(Situation(situation.rule, situation.divider + 1, situation.index))
+        for situation in self.D[j - 1]:
+            try:
+                if situation.rule.second[situation.divider] == self.word[j - 1]:
+                    self.D[j].add(Situation(situation.rule, situation.divider + 1, situation.index))
+            except Exception:
+                pass
 
+    def Predict(self, j):
+        counter = 0
+        for situation in self.D[j]:
+            if situation.divider >= len(situation.rule.second):
+                continue
+            for rule in self.grammar:
+                try:
+                    if rule.first == situation.rule.second[situation.divider]:
+                        s = Situation(rule, 0, j)
+                        if s not in self.D[j]:
+                            self.D[j].add(s)
+                            counter += 1
+                except Exception:
+                    pass
+        return counter
+
+    def Complete(self, j):
+        counter = 0
+        for situation in self.D[j]:
+            if situation.divider != len(situation.rule.second):
+                continue
+            for second_situation in self.D[situation.index]:
+                try:
+                    if situation.rule.first == second_situation.rule.second[second_situation.divider]:
+                        s = Situation(second_situation.rule, second_situation.divider + 1, second_situation.index)
+                        if s not in self.D[j]:
+                            self.D[j].add(s)
+                            counter += 1
+                except Exception:
+                    pass
+        return counter
+
+#        def Earley(self):
+#            self.D[0].add(Situation(Rule('S1', 'S'), 0, 0))
+#            for j in range(len(self.word) + 1):
+#                self.Scan(j)
+#                ...
+#            if Situation(Rule('S1', 'S'), 1, 0) in self.D[len(self.word)]:
+#                return True
+#            else:
+#                return False
 
 
 if __name__ == '__main__':
     grammar = [Rule('S', 'Sa'), Rule('S', 'Sb'), Rule('S', 'C'), Rule('C', 'Dd'), Rule('D', 'cD'), Rule('D', '')]
     word = 'cdba'
-    #res = Earley.Check(grammar, word)
-    #print(res)
+    algo = AlgorithmEarley(grammar, word)
+    print(algo.Earley())
